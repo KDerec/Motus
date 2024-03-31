3a720ea600a392f672c2924bb2544c2ab8db2b26
Kévin Dérécusson <kevin.derecusson@outlook.fr>
2024-03-31 18:45:52 +0200

    ✨ User can change password

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "password-change/",
+        auth_views.PasswordChangeView.as_view(
+            success_url=reverse_lazy("password_change_done")
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
 ]
