adfa94d5bdde7d8afdf0b0a348ba6eb8aad067ef
Kévin Dérécusson <kevin.derecusson@outlook.fr>
2024-04-01 20:13:49 +0200

    🗑️ Delete WordGenerator
    the used api don't work anymore

--- motus/views.py
+++ motus/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import WordGenerator
+from .models import WordToGuess
 
 
 @login_required
 def home(request):
-    word_generator = WordGenerator()
-    new_word = word_generator.get_new_word(5)
-    return render(request, "home.html", {"new_word": new_word})
+    return render(request, "home.html")
 
 
 def start_game(request):
@@ -15,6 +13,9 @@ def start_game(request):
         difficulty = request.POST.get("difficulty")
         if difficulty is None or difficulty == "" or int(difficulty) < 3:
             return redirect("motus:home")
-        return render(request, "game.html", {"word_length": difficulty})
+        word = "test"
+        word = WordToGuess(word_text=word)
+        word.save()
+        return render(request, "game.html", {"word_length": difficulty, "word": word})
     else:
         return redirect("motus:home")
