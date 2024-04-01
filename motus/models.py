ee935839b547291c399e0e4b2c855419c13dfbbb
Kévin Dérécusson <kevin.derecusson@outlook.fr>
2024-04-01 13:17:39 +0200

    ✨ Generate random new word

--- motus/models.py
+++ motus/models.py
@@ -1,6 +1,24 @@
+import requests
+
 # from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
     pass
+
+
+class WordGenerator:
+    def __init__(self):
+        self.base_link = "https://random-word-api.herokuapp.com/word?lang=fr&&"
+
+    def get_new_word(self, length):
+        response = requests.get(self.base_link + f"length={length}")
+        if response.status_code == 200:
+            word = self.format_response(response.text)
+            return word
+        else:
+            raise Exception(f"Erreur {response.status_code} : {response.text}")
+
+    def format_response(self, response):
+        return response.replace("[", "").replace("]", "").replace('"', "")
--- motus/views.py
+++ motus/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import WordGenerator
 
 
 @login_required
 def home(request):
-    return render(request, "home.html")
+    word_generator = WordGenerator()
+    new_word = word_generator.get_new_word(5)
+    return render(request, "home.html", {"new_word": new_word})
