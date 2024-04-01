1ebfaa63a20508fe12e70517a2b8268e663b92d1
Kévin Dérécusson <kevin.derecusson@outlook.fr>
2024-04-01 14:25:06 +0200

    🥅 Verify difficulty value

--- motus/views.py
+++ motus/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import WordGenerator
 
@@ -13,4 +13,8 @@ def home(request):
 def start_game(request):
     if request.method == "POST":
         difficulty = request.POST.get("difficulty")
+        if difficulty is None or difficulty == "" or int(difficulty) < 3:
+            return redirect("motus:home")
         return render(request, "game.html", {"word_length": difficulty})
+    else:
+        return redirect("motus:home")
