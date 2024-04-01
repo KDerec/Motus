dadc546255d1ce0553c74c0c98ec75df6008c9d0
Kévin Dérécusson <kevin.derecusson@outlook.fr>
2024-04-01 13:51:37 +0200

    ✨ User can select difficulty

--- motus/urls.py
+++ motus/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, start_game
 
 urlpatterns = [
     path("", home, name="home"),
+    path("game", start_game, name="start_game"),
 ]
--- motus/views.py
+++ motus/views.py
@@ -8,3 +8,9 @@ def home(request):
     word_generator = WordGenerator()
     new_word = word_generator.get_new_word(5)
     return render(request, "home.html", {"new_word": new_word})
+
+
+def start_game(request):
+    if request.method == "POST":
+        difficulty = request.POST.get("difficulty")
+        return render(request, "game.html", {"word_length": difficulty})
