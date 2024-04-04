8935ffdde38265a85b6807d13f848c5e9790f0f9
Kévin Dérécusson <kevin.derecusson@outlook.fr>
2024-04-04 09:30:51 +0200

    🚧 Add game model

--- motus/admin.py
+++ motus/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, WordToGuess
+from .models import User, WordToGuess, Game
 
 
 admin.site.register(User, UserAdmin)
 admin.site.register(WordToGuess)
+admin.site.register(Game)
--- motus/models.py
+++ motus/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import UniqueConstraint
 from django.contrib.auth.models import AbstractUser
 
 
@@ -11,3 +12,16 @@ class WordToGuess(models.Model):
 
     def __str__(self):
         return f"{self.word_text}"
+
+
+class Game(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    life_point = models.IntegerField(default=6)
+
+    def __str__(self):
+        return f"{self.user, self.life_point}"
+
+    class Meta:
+        constraints = [
+            UniqueConstraint(fields=["user"], name="unique_game_per_user"),
+        ]
