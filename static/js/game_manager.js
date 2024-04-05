0a9f8f328d8637e18079fbfdf02565597309bdbd
Kévin Dérécusson <kevin.derecusson@outlook.fr>
2024-04-05 12:28:01 +0200

    🎨 Autoformat js file

--- static/js/game_manager.js
+++ static/js/game_manager.js
@@ -5,19 +5,19 @@ const wordId = document.getElementById('word-id');
 const endGameMessage = document.getElementById('endgame-message');
 
 function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
+  let cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim();
+      // Does this cookie string begin with the name we want?
+      if (cookie.substring(0, name.length + 1) === (name + '=')) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+        break;
+      }
     }
-    return cookieValue;
+  }
+  return cookieValue;
 }
 const csrftoken = getCookie('csrftoken');
 
@@ -45,14 +45,14 @@ guessForm.addEventListener('submit', async (event) => {
   const win = data.data.win;
   const run = data.data.run
 
-  if (win == true){
+  if (win == true) {
     guessForm.remove();
     gridContainer.remove();
     var content = document.createTextNode(`Bravo ! Vous avez trouvé le mot : ${guess} (+${guess.length} points) !`);
     endGameMessage.appendChild(content);
   };
 
-  if (run == false){
+  if (run == false) {
     guessForm.remove();
     gridContainer.remove();
     var word = data.data.word
@@ -65,15 +65,15 @@ guessForm.addEventListener('submit', async (event) => {
 });
 
 function updateGrid(guess, colorList) {
-    const newRow = document.createElement('tr');
+  const newRow = document.createElement('tr');
 
-    // Iterate through guess and color list to create cells
-    for (let i = 0; i < guess.length; i++) {
-        const cell = document.createElement('td');
-        cell.textContent = guess[i];
-        cell.classList.add(colorList[i]);
-        newRow.appendChild(cell);
-    }
+  // Iterate through guess and color list to create cells
+  for (let i = 0; i < guess.length; i++) {
+    const cell = document.createElement('td');
+    cell.textContent = guess[i];
+    cell.classList.add(colorList[i]);
+    newRow.appendChild(cell);
+  }
 
-    gridContainer.appendChild(newRow);
+  gridContainer.appendChild(newRow);
 }
